52b928bf188dd966bcf435d7ffec16a44a04c70d
Thales Alves <thalesalves758@gmail.com>
2023-01-18 16:27:31 -0300

    use car repository to save and list all cars

--- api/src/main/java/com/carsapi/api/controller/CarController.java
+++ api/src/main/java/com/carsapi/api/controller/CarController.java
@@ -1,20 +1,31 @@
 package com.carsapi.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carsapi.api.dto.CarDTO;
+import com.carsapi.api.model.Car;
+import com.carsapi.api.repository.CarRepository;
 
 @RestController
 @RequestMapping("/cars")
 public class CarController {
+  @Autowired
+  private CarRepository carRepository;
+
   @PostMapping()
   public void receiveCar(@RequestBody CarDTO reqBody) {
-    System.out.println(reqBody.modelo());
-    System.out.println(reqBody.fabricante());
-    System.out.println(reqBody.valor());
+    carRepository.save(new Car(reqBody));
+  }
 
+  @GetMapping()
+  public List<Car> listAll() {
+    return carRepository.findAll();
   }
 }
