3ea3505881b823764c6af2a460cfd9ca31ddd2e1
Thales Alves <thalesalves758@gmail.com>
2023-01-20 09:11:04 -0300

    create update car route

--- api/src/main/java/com/carsapi/api/controller/CarController.java
+++ api/src/main/java/com/carsapi/api/controller/CarController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,8 @@ import com.carsapi.api.dto.CarDTO;
 import com.carsapi.api.model.Car;
 import com.carsapi.api.repository.CarRepository;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/cars")
 public class CarController {
@@ -22,7 +25,7 @@ public class CarController {
   private CarRepository carRepository;
 
   @PostMapping()
-  public void receiveCar(@RequestBody CarDTO reqBody) {
+  public void receiveCar(@RequestBody @Valid CarDTO reqBody) {
     carRepository.save(new Car(reqBody));
   }
 
@@ -34,5 +37,17 @@ public class CarController {
   @DeleteMapping("/{id}")
   public void deleteCar(@PathVariable Long id) {
     carRepository.deleteById(id);
+  }
+
+  @PutMapping("/{id}")
+  public void updateCar(@PathVariable Long id, @RequestBody @Valid CarDTO reqBody) {
+    carRepository.findById(id).map(car -> {
+      car.setModelo(reqBody.modelo());
+      car.setAnoModelo(reqBody.anoModelo());
+      car.setDataFabricacao(reqBody.dataFabricacao());
+      car.setFabricante(reqBody.fabricante());
+      car.setValor(reqBody.valor());
+      return carRepository.save(car);
+    });
   }
 }
