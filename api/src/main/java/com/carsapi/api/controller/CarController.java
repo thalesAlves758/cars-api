7598f7a5aba4069cb44465d32c3c9da7635271b4
Thales Alves <thalesalves758@gmail.com>
2023-01-20 08:56:09 -0300

    create delete car route

--- api/src/main/java/com/carsapi/api/controller/CarController.java
+++ api/src/main/java/com/carsapi/api/controller/CarController.java
@@ -3,7 +3,9 @@ package com.carsapi.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +29,10 @@ public class CarController {
   @GetMapping()
   public List<Car> listAll() {
     return carRepository.findAll();
+  }
+
+  @DeleteMapping("/{id}")
+  public void deleteCar(@PathVariable Long id) {
+    carRepository.deleteById(id);
   }
 }
